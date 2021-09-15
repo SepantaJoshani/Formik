71131a9ac6fe577ef4ab3bdf292a95190a5233d7
Sepanta <Sepanta_97@Yahoo.com>
2021-09-15 10:25:20 +0430

    value and onchange with formik

--- src/components/OldForm.js
+++ src/components/OldForm.js
@@ -1,23 +1,50 @@
 import React from "react";
-
+import { useFormik } from "formik";
 
 function OldForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+
+  console.log(formik.values);
   return (
     <div>
       <form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" name="channel" />
+          <input
+            type="text"
+            id="channel"
+            name="channel"
+            onChange={formik.handleChange}
+            value={formik.values.channel}  
+          />
         </div>
 
         <button
