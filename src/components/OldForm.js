9aba821d41371aafad4af394f3c92ed3ac747b00
Sepanta <Sepanta_97@Yahoo.com>
2021-09-16 16:54:20 +0430

    list numbers

--- src/components/OldForm.js
+++ src/components/OldForm.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import ErrorTxt from './ErrorTxt'
+import { ErrorMessage, Field, FieldArray, Form, Formik } from "formik";
+import ErrorTxt from "./ErrorTxt";
 import * as Yup from "yup";
 
 const initialValues = {
   name: "",
   email: "",
   channel: "",
-  comments:"",
-  address:"",
-  social:{
-    facebook:'',
-    yahoo:'',
-  }
+  comments: "",
+  address: "",
+  social: {
+    facebook: "",
+    yahoo: "",
+  },
+  phoneNubmers: [""],
 };
 
 const onSubmit = (values) => console.log(values);
@@ -21,7 +22,7 @@ const validationSchema = Yup.object({
   name: Yup.string().required("Required !"),
   email: Yup.string().email("Invalid Email Format").required("Required !"),
   channel: Yup.string().required("Required !"),
-  address:Yup.string().required('Required !')
+  address: Yup.string().required("Required !"),
 });
 
 function OldForm() {
@@ -42,46 +43,77 @@ function OldForm() {
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
           <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email" >
-            {
-              errorMessage => <p className="error">{errorMessage}</p>
-            }
+          <ErrorMessage name="email">
+            {(errorMessage) => <p className="error">{errorMessage}</p>}
           </ErrorMessage>
         </div>
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
           <Field type="text" id="channel" name="channel" />
-          <ErrorMessage name="channel"  />
+          <ErrorMessage name="channel" />
         </div>
         <div className="form-control">
           <label htmlFor="comments">Comments</label>
           <Field as="textarea" id="comments" name="comments" />
         </div>
-       <div className="form-control">
-         <label htmlFor="address">Address</label>
-           <Field name="address" >
-              {
-                (props)=>{
-                  const {field,meta,form}=props
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, meta, form } = props;
 
-                  return <div>
-                    <input type="text" id="address" {...field} />
-                    {meta.touched && meta.error && <p className="error">{meta.error}</p>}
-                  </div>
-                }
-              }
-           </Field>
-        
-       </div>
-       <div className="form-control">
-         <label htmlFor="facebook">Facebook Profile</label>
-         <Field type="text" id="facebook" name='social.facebook' />
-       </div>
-       <div className="form-control">
-         <label htmlFor="yahoo">yahoo Profile</label>
-         <Field type="text" id="yahoo" name='social.yahoo' />
-       </div>
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error && (
+                    <p className="error">{meta.error}</p>
+                  )}
+                </div>
+              );
+            }}
+          </Field>
+        </div>
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook Profile</label>
+          <Field type="text" id="facebook" name="social.facebook" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="yahoo">yahoo Profile</label>
+          <Field type="text" id="yahoo" name="social.yahoo" />
+        </div>
+
+        <div className="form-control">
+          <label>List Of Numbers</label>
+          <FieldArray name="phoneNubmers">
+            {(fieldArrayProps) => {
+              console.log(fieldArrayProps);
+              const { form, push, remove } = fieldArrayProps;
+              const { values } = form;
+              const { phoneNubmers } = values;
+
+              return (
+                <div>
+                  {phoneNubmers.map((phoneNumber, index) => (
+                    <div key={index}>
+                      <Field name={`phoneNumbers[${index}]`} />
+                      {index > 0 && (
+                        <button type="button" onClick={() => remove(index)}>
+                          -
+                        </button>
+                      )}
+                      {index <= 5 && (
+                        <button type="button" onClick={() => push("")}>
+                          +
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
+        </div>
 
         <button
           type="submit"
